ff63f4890b66dc956ee433c0f492458c4ef25c19
Maxime Taisant <maximetaisant@hotmail.fr>
2017-08-24 13:11:48 +0200

    ...

--- libavcodec/jpeg2000dwt.c
+++ libavcodec/jpeg2000dwt.c
@@ -30,6 +30,7 @@
 #include "libavutil/mem.h"
 #include "jpeg2000dwt.h"
 #include "internal.h"
+#include "libavutil/timer.h"
 
 /* Defines for 9/7 DWT lifting parameters.
  * Parameters are in float. */
@@ -558,7 +559,7 @@ int ff_jpeg2000_dwt_init(DWTContext *s, int border[2][2],
         }
     switch (type) {
     case FF_DWT97:
-        s->f_linebuf = av_malloc_array((maxlen + 12), sizeof(*s->f_linebuf));
+        s->f_linebuf = av_malloc_array(6*(maxlen + 12), sizeof(*s->f_linebuf));
         if (!s->f_linebuf)
             return AVERROR(ENOMEM);
         break;
@@ -609,9 +610,21 @@ int ff_dwt_decode(DWTContext *s, void *t)
     switch(s->type){
         case FF_DWT97:
             if (s->sse)
+            //{
+            //    START_TIMER
                 dwt_decode97_float_sse(s, t);
+            //    STOP_TIMER("dwt_decode97_float_sse");
+            //}
             else            
-                dwt_decode97_float(s, t); 
+            //{
+            //    START_TIMER
+                dwt_decode97_float(s, t);
+            //    STOP_TIMER("dwt_decode97_float");
+            //}
+            /*{
+                START_TIMER
+                STOP_TIMER("decode_NULL");
+            }*/
             break;
         case FF_DWT97_INT:
             dwt_decode97_int(s, t); break;
